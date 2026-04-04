6be613d1cfa36035ad12bb32bfd0fcfdea51327a
KYS <ehcl1027@gmail.com>
2026-04-04 18:55:32 +0900

    chore : add implements & fix Resolver

--- src/main/java/com/caring/common/resolver/MemberCodeArgumentResolver.java
+++ src/main/java/com/caring/common/resolver/MemberCodeArgumentResolver.java
@@ -1,14 +1,13 @@
 package com.caring.common.resolver;
 
 import com.caring.common.annotation.UserCode;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-
-import static com.caring.common.consts.StaticVariable.REQUEST_HEADER_USERNAME;
 
 
 public class MemberCodeArgumentResolver implements HandlerMethodArgumentResolver {
@@ -21,8 +20,8 @@ public class MemberCodeArgumentResolver implements HandlerMethodArgumentResolver
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        return request.getHeader(REQUEST_HEADER_USERNAME);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null ? authentication.getName() : null;
     }
 
     private boolean isSupportedAnnotation(MethodParameter parameter) {
